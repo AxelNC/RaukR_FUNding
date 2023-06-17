fc2b84c74661b8875f2f90dbb6f16a4eaa3240f7
AxelNC <axel.cederholm@imbim.uu.se>
2023-06-17 16:43:45 +0200

    cleaned and hopefully with all dependencies

--- Axel_map_demo3.R
+++ Axel_map_demo3.R
@@ -1,31 +1,19 @@
-#Map demo from https://github.com/reinholdsson/swemaps/blob/master/README.md readme
-# install.packages('devtools')
-#devtools::install_github('reinholdsson/swemaps')
+#map demo 3 insipred by https://github.com/reinholdsson/swemaps/blob/master/README.md
 library(tidyverse)
-library(rkolada)  # devtools::install_github("reinholdsson/rkolada")
-# rkolada conn
-a <- rkolada::rkolada()
-
 library(swemaps)
 library(leaflet)  # devtools::install_github("rstudio/leaflet")
 
-
-
-
-library(plotrix) #install.packages("plotrix")
-
-
-x <- a$values('N01963', year = 2010)
-x <- subset(x, gender == 'T')
-x <- prepare_map_data(x)
-x$color <- substring(color.scale(x$value, c(1,1,0), c(0,1,1), 0), 1, 7)
+x <- map_kn
 
 m <- leaflet() %>% addTiles()
+
 for (kn in unique(x$knkod)) {
   i <- x[x$knkod == kn,]
-  m <- m %>% addPolygons(i$leaflet_long, i$leaflet_lat, color = i$color[[1]], weight = 1)
+  m <- m %>% addPolygons(i$leaflet_long, i$leaflet_lat, color = 'blue', weight = 1)
 }
-m
+
+m  # plot!
+
 
 
 ####Make sure to change value and then try to create a continious color scale according to the below example and implement it as in the second example below
